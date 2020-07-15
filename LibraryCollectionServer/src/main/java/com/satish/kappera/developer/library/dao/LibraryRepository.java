b15ed9a035c19cf735a2f5a43d0edd5a74d43b88
satish kumar kappera <satish.kappera@esystems-inc.com>
2020-07-15 13:28:14 -0400

    Update LibraryRepository.java
    2nd comment

--- LibraryCollectionServer/src/main/java/com/satish/kappera/developer/library/dao/LibraryRepository.java
+++ LibraryCollectionServer/src/main/java/com/satish/kappera/developer/library/dao/LibraryRepository.java
@@ -9,4 +9,5 @@ import com.satish.kappera.developer.library.model.Library;
 @Repository
 public interface LibraryRepository extends JpaRepository<Library, Long> {
     Library findByName(String name);
+	//2nd comment
 }
